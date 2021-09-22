c3a12a1a321ce401b7c054a168258381d9e17ec6
nancinewell <nanci.newell@gmail.com>
2021-09-21 21:17:46 -0500

    update

--- cse341-project-master/dist/prove01-routes.dev.js
+++ cse341-project-master/dist/prove01-routes.dev.js
@@ -1,24 +1,30 @@
 "use strict";
 
-var userNames = [];
+var userNames = ["mreynolds", "zwashburne", "hwashburne", "jcobb", "kfrye"];
+var firstNames = ["Malcom", "Zoe", "Hoburn", "Jayne", "Kaylee"];
+var lastNames = ["Reynolds", "Washburne", "Washburne", "Cobb", "Frye"];
 
 var requestHandler = function requestHandler(req, res) {
   var url = req.url;
   var method = req.method;
-
-  if (userNames.length == 0) {
-    userNames.push("mreynolds", "zwashburne", "hwashburne", "jcobb", "kfrye");
-  }
+  /*if(userNames.length == 0){
+      userNames.push("mreynolds", "zwashburne", "hwashburne", "jcobb", "kfrye");
+  }*/
 
   if (url === "/") {
     res.write('<html>');
     res.write('<head><title>Greetings!</title></head>');
-    res.write('<body style="font-family: sans-serif; line-height: 1.5em;"><h1 style="line-height: 1.75em;">Greetings! Please enter your username.</h1><form action="/create-user" method="POST"><input name="username" type="text" placeholder="Alpha-numeric only." style="border-radius: 5px; margin: 1em; padding: .5em;"><button type="submit" style="font-family: sans-serif; padding: .5em; border-radius: 5px;">Submit</button></form><ul>');
-    userNames.forEach(function (user) {
-      res.write("<li style=\"list-style-type: none;\">".concat(user, "</li>"));
-    });
-    res.write('</ul></body>');
-    res.write('</html>');
+    res.write('<body style="font-family: sans-serif; line-height: 1.5em; margin-left: 1.5em;">' + '<h1 style="line-height: 1.75em;">Thanks for joining us!</h1>' + '<h2>Please enter your information below.</h2>' + '<form action="/create-user" method="POST" style="max-width: 350px;">' + '<fieldset style="max-width: 350px; border-radius: 5px; display: grid; grid-template-columns: 1fr 2fr; ">' + '<legend>Sign Up</legend>' + '<label for="firstname">First Name</label>' + '<input name="firstname" type="text" placeholder="Malcom" required title="Letters only please." style="border-radius: 5px; margin-bottom: 1em; padding: .5em;">' + '<label for="lastname">Last Name</label>' + '<input name="lastname" type="text" placeholder="Reynolds" required pattern="[A-Za-z]" title="Letters only please." style="border-radius: 5px; margin-bottom: 1em; padding: .5em;">' + '<label for="username">User Name</label>' + '<input name="username" type="text" placeholder="jdoe" required pattern="[A-Za-z0-9]+" title="Letters and numbers only please." style="border-radius: 5px; margin-bottom: 1em; padding: .5em;" pattern="[A-Za-z0-9]">' + '<br>' + '<button type="submit" style="font-family: sans-serif; padding: .5em; width: 100%; border-radius: 5px; ">Submit</button>' + '</fieldset></form>');
+    /*+'<br><h2>Existing Users</h2>');
+      res.write('<table><tr><th style="padding-right: .75em;">First Name</th><th style="padding-right: .75em;">Last Name</th><th style="padding-right: .75em;">Username</th></tr>');
+    for(i=0; i<userNames.length; i++){
+        res.write(`<tr><td>${firstNames[i]}</td>`);
+        res.write(`<td>${lastNames[i]}</td>`);
+        res.write(`<td>${userNames[i]}</td></tr>`);
+    }
+          res.write('</table></body>');*/
+
+    res.write('</body></html>');
     return res.end();
   }
 
@@ -29,9 +35,23 @@ var requestHandler = function requestHandler(req, res) {
     });
     return req.on('end', function () {
       var parsedBody = Buffer.concat(body).toString();
-      var user = parsedBody.split('=')[1];
-      userNames.push(user);
-      listUsers(res, "Users");
+      console.log(parsedBody);
+      var userInfo = parsedBody.split("&");
+      var fName = userInfo[0].split("=")[1];
+      var lName = userInfo[1].split("=")[1];
+      var uName = userInfo[2].split("=")[1];
+      console.log(fName, lName, uName);
+      firstNames.push(fName);
+      lastNames.push(lName);
+      userNames.push(uName);
+      res.setHeader('Content-Type', 'text/html');
+      res.write('<html>');
+      res.write('<head><title>Prove01 - Nanci Newell</title></head>');
+      res.write("<body style='font-family: sans-serif; line-height: 1.5em;'><h1 style='line-height: 1.75em; margin-left: 1.5em;'>Welcome, ".concat(fName, "!</h1>"));
+      res.write('<table style="margin-left: 1.5em;"><tr><th style="padding-right: .75em;">First Name</th><th style="padding-right: .75em;">Last Name</th><th style="padding-right: .75em;">Username</th></tr>');
+      res.write("<tr><td>".concat(fName, "</td><td>").concat(lName, "</td><td>").concat(uName, "</td></tr>"));
+      res.write('</table></body>');
+      res.write('</html>');
       res.end();
     });
   }
@@ -41,7 +61,7 @@ var requestHandler = function requestHandler(req, res) {
     return res.end();
   }
 
-  listUsers(res, "That wasn't a valid route, but you can see the users here.");
+  listUsers(res, "That wasn't a valid route.");
   res.end();
 };
 
@@ -49,11 +69,16 @@ function listUsers(res, h1) {
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>Prove01 - Nanci Newell</title></head>');
-  res.write("<body style='font-family: sans-serif; line-height: 1.5em;'><h1 style='line-height: 1.75em;'>".concat(h1, "</h1><ul>"));
-  userNames.forEach(function (user) {
-    res.write("<li style=\"list-style-type: none;\">".concat(user, "</li>"));
-  });
-  res.write('</ul></body>');
+  res.write("<body style='font-family: sans-serif; line-height: 1.5em;'><h1 style='line-height: 1.75em; margin-left: 1.5em;'>".concat(h1, "</h1>"));
+  res.write('<table style="margin-left: 1.5em;"><tr><th style="padding-right: .75em;">First Name</th><th style="padding-right: .75em;">Last Name</th><th style="padding-right: .75em;">Username</th></tr>');
+
+  for (i = 0; i < userNames.length; i++) {
+    res.write("<tr><td>".concat(firstNames[i], "</td>"));
+    res.write("<td>".concat(lastNames[i], "</td>"));
+    res.write("<td>".concat(userNames[i], "</td></tr>"));
+  }
+
+  res.write('</table></body>');
   res.write('</html>');
 }
 
